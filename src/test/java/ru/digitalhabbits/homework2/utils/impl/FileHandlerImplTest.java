c82326a429abebd62913df9965c96016577da695
Dmitry Kopylov <dimayellow@gmail.com>
2022-04-25 22:53:05 +0300

    Revert "bug fix"
    This reverts commit d4add22dbad00110c9b9ed9a35fd7bec9ef2c83a.

--- src/test/java/ru/digitalhabbits/homework2/utils/impl/FileHandlerImplTest.java
+++ src/test/java/ru/digitalhabbits/homework2/utils/impl/FileHandlerImplTest.java
@@ -15,16 +15,16 @@ class FileHandlerImplTest {
     @Test
     public void whenSendFile_correctlyHandleAllLines() throws Exception {
 
-        LineHandler testHandler = Mockito.mock(LineHandler.class);
-
         var aStringMock = getHandleMock(firstLineMap());
-        Mockito.when(testHandler.getNewHandler("aaab")).thenReturn(aStringMock);
-
         var bStringMock = getHandleMock(secondLineMap());
-        Mockito.when(testHandler.getNewHandler("gggss")).thenReturn(bStringMock);
-
         var cStringMock = getHandleMock(thirdLineMap());
+
+
+        LineHandler testHandler = Mockito.mock(LineHandler.class);
+        Mockito.when(testHandler.getNewHandler("aaab")).thenReturn(aStringMock);
+        Mockito.when(testHandler.getNewHandler("gggss")).thenReturn(bStringMock);
         Mockito.when(testHandler.getNewHandler("asw")).thenReturn(cStringMock);
+
 
         var file = new File(getResource("testFrileReaderImpl").getPath());
         var testObject = new FileHandlerImpl(testHandler);
@@ -39,9 +39,9 @@ class FileHandlerImplTest {
     }
 
     private LineHandler getHandleMock(Map<Character, Long> characterLongMap) throws Exception {
-        LineHandler mock = Mockito.mock(LineHandler.class);
-        Mockito.when(mock.call()).thenReturn(characterLongMap);
-        return mock;
+        LineHandler aStringHandler = Mockito.mock(LineHandler.class);
+        Mockito.when(aStringHandler.call()).thenReturn(characterLongMap);
+        return aStringHandler;
     }
 
     private Map<Character, Long> firstLineMap() {
